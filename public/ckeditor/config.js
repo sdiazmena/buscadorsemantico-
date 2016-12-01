2c96ba8b52d1360714e70ead24653262685481f3
sdiazmena <diazmenasebastian@hotmail.com>
2016-12-01 20:36:14 -0300

    Add files via upload

---
+++ public/ckeditor/config.js
@@ -0,0 +1,11 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	config.language = 'es';
+	config.newpage_html = '<p>Type your text here.</p>';
+	config.menu_groups = 'clipboard,table,anchor,link,image';
+};
